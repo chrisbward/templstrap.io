783c14d8fc80817742c99537a11d8ee4ee99c1db
user <user@email>
2024-05-16 03:44:17 +0100

    reverted

--- pkg/base/base_templ.go
+++ pkg/base/base_templ.go
@@ -43,10 +43,6 @@ func Show(props BaseProps) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = props.BodyContentComponent.Render(ctx, templ_7745c5c3_Buffer)
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
 		templ_7745c5c3_Err = templ_7745c5c3_Var1.Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
