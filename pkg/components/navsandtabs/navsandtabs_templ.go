060d7a0ff2c37a1ba2bd06204aa49b25c746aab5
user <user@email>
2024-05-22 22:22:37 +0100

    navsandtabs updated

--- pkg/components/navsandtabs/navsandtabs_templ.go
+++ pkg/components/navsandtabs/navsandtabs_templ.go
@@ -10,7 +10,51 @@ import "context"
 import "io"
 import "bytes"
 
-func Show() templ.Component {
+import (
+	"github.com/chrisbward/templstrap.io/pkg/base"
+	"github.com/chrisbward/templstrap.io/pkg/components/navbar/navitem"
+	"strings"
+)
+
+const RootClassName = "nav"
+
+type NavTabStyle string
+
+const TabStyle NavTabStyle = "nav-tabs"
+const PillStyle NavTabStyle = "nav-pills"
+const UnderlineStyle NavTabStyle = "nav-underline"
+
+type HorizontalAlignment string
+
+const AlignDefault HorizontalAlignment = ""
+const AlignCenter HorizontalAlignment = "justify-content-center"
+const AlignEnd HorizontalAlignment = "justify-content-end"
+
+type NavsAndTabsProps struct {
+	base.FormElementProps
+	NavItems            []*navitem.NavItemProps
+	TabStyle            NavTabStyle
+	IsVertical          bool
+	HorizontalAlignment HorizontalAlignment
+	ExtraAttributes     map[string]any
+}
+
+func (natp NavsAndTabsProps) BuildClassNames() (classes string) {
+	classNames := []string{RootClassName}
+
+	classNames = append(classNames, string(natp.TabStyle))
+	classNames = append(classNames, string(natp.HorizontalAlignment))
+
+	if natp.IsVertical {
+		classNames = append(classNames, "flex-column")
+	}
+
+	classes = strings.Join(classNames, " ")
+
+	return
+}
+
+func Show(props NavsAndTabsProps) templ.Component {
 	return templ.ComponentFunc(func(ctx context.Context, templ_7745c5c3_W io.Writer) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templ_7745c5c3_W.(*bytes.Buffer)
 		if !templ_7745c5c3_IsBuffer {
@@ -23,7 +67,42 @@ func Show() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<ul class=\"nav\"><li class=\"nav-item\"><a class=\"nav-link active\" aria-current=\"page\" href=\"#\">Active</a></li><li class=\"nav-item\"><a class=\"nav-link\" href=\"#\">Link</a></li><li class=\"nav-item\"><a class=\"nav-link\" href=\"#\">Link</a></li><li class=\"nav-item\"><a class=\"nav-link disabled\" aria-disabled=\"true\">Disabled</a></li></ul>")
+		var templ_7745c5c3_Var2 = []any{props.BuildClassNames()}
+		templ_7745c5c3_Err = templ.RenderCSSItems(ctx, templ_7745c5c3_Buffer, templ_7745c5c3_Var2...)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<ul class=\"")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		var templ_7745c5c3_Var3 string
+		templ_7745c5c3_Var3, templ_7745c5c3_Err = templ.JoinStringErrs(templ.CSSClasses(templ_7745c5c3_Var2).String())
+		if templ_7745c5c3_Err != nil {
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `pkg/components/navsandtabs/navsandtabs.templ`, Line: 1, Col: 0}
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var3))
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\">")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		for _, NavItem := range props.NavItems {
+			templ_7745c5c3_Err = navitem.Show(navitem.NavItemProps{
+				Name:          NavItem.Name,
+				Link:          NavItem.Link,
+				IsActive:      NavItem.IsActive,
+				IsDisabled:    NavItem.IsDisabled,
+				IsDropdown:    NavItem.IsDropdown,
+				ChildNavItems: NavItem.ChildNavItems,
+			}).Render(ctx, templ_7745c5c3_Buffer)
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</ul>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
