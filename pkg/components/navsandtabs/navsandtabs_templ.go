4d2324f55b6ff8ab559fca4c02427c428d178d7a
user <user@email>
2024-05-22 22:25:39 +0100

    forgot to add children

--- pkg/components/navsandtabs/navsandtabs_templ.go
+++ pkg/components/navsandtabs/navsandtabs_templ.go
@@ -102,6 +102,10 @@ func Show(props NavsAndTabsProps) templ.Component {
 				return templ_7745c5c3_Err
 			}
 		}
+		templ_7745c5c3_Err = templ_7745c5c3_Var1.Render(ctx, templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</ul>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
