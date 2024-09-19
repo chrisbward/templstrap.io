cc2253805eda084135eb926a74c04d22cd6e78db
user <user@email>
2024-09-19 16:12:36 +0100

    updated badge component

--- pkg/components/badge/badge_templ.go
+++ pkg/components/badge/badge_templ.go
@@ -8,7 +8,51 @@ package badge
 import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
-func Show() templ.Component {
+import (
+	"github.com/chrisbward/templstrap.io/pkg/base"
+	"strings"
+)
+
+type BadgeClassType string
+
+const PrimaryBadge BadgeClassType = "text-bg-primary"
+const SecondaryBadge BadgeClassType = "text-bg-secondary"
+const SuccessBadge BadgeClassType = "text-bg-success"
+const DangerBadge BadgeClassType = "text-bg-danger"
+const WarningBadge BadgeClassType = "text-bg-warning"
+const InfoBadge BadgeClassType = "text-bg-info"
+const LightBadge BadgeClassType = "text-bg-light"
+const DarkBadge BadgeClassType = "text-bg-dark"
+
+type BadgeProps struct {
+	base.ElementProps
+	Text           string
+	IsRoundedPill  bool
+	BadgeClassType BadgeClassType
+}
+
+var RootClassName = "badge"
+
+func (bp BadgeProps) BuildClassNames() (classes string) {
+	classValues := []string{RootClassName}
+
+	classValues = append(classValues, "needs-validation")
+	classValues = append(classValues, string(bp.BadgeClassType))
+
+	if bp.IsRoundedPill {
+		classValues = append(classValues, "rounded-pill")
+	}
+
+	if bp.AdditionalClasses != nil {
+		classValues = append(classValues, bp.AdditionalClasses...)
+	}
+
+	classes = strings.Join(classValues, " ")
+
+	return
+}
+
+func Show(props BadgeProps) templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
 		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templruntime.GetBuffer(templ_7745c5c3_W)
@@ -26,7 +70,38 @@ func Show() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<span class=\"badge bg-secondary\">val</span>")
+		var templ_7745c5c3_Var2 = []any{props.BuildClassNames()}
+		templ_7745c5c3_Err = templ.RenderCSSItems(ctx, templ_7745c5c3_Buffer, templ_7745c5c3_Var2...)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<span class=\"")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		var templ_7745c5c3_Var3 string
+		templ_7745c5c3_Var3, templ_7745c5c3_Err = templ.JoinStringErrs(templ.CSSClasses(templ_7745c5c3_Var2).String())
+		if templ_7745c5c3_Err != nil {
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `pkg/components/badge/badge.templ`, Line: 1, Col: 0}
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var3))
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\">")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		var templ_7745c5c3_Var4 string
+		templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinStringErrs(props.Text)
+		if templ_7745c5c3_Err != nil {
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `pkg/components/badge/badge.templ`, Line: 50, Col: 20}
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var4))
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</span>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
