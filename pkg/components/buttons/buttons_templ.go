4036971103d5c45a2c475238d64d629f9b665cdd
user <user@email>
2024-12-20 05:14:46 +0000

    addition of htmx properties to anchor for button component

--- pkg/components/buttons/buttons_templ.go
+++ pkg/components/buttons/buttons_templ.go
@@ -208,6 +208,10 @@ func Show(props ButtonProps) templ.Component {
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
+			templ_7745c5c3_Err = templ.RenderAttributes(ctx, templ_7745c5c3_Buffer, props.HTMX.GetAttributes())
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(">")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
