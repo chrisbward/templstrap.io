ad4104116c346a472b638383d25be9be3246556e
user <user@email>
2025-05-13 01:06:32 +0100

    debugging pagination

--- pkg/components/pagination/paginationitem/paginationitem_templ.go
+++ pkg/components/pagination/paginationitem/paginationitem_templ.go
@@ -43,7 +43,9 @@ func (pp PaginationItemProps) BuildClassName(isActive bool, isDisabled bool) (cl
 
 func (pp PaginationItemProps) RebuildHTMXAttributes() (htmxAttributes map[string]any) {
 	htmxAttributes = make(map[string]any)
-	htmxAttributes = pp.HTMX.GetAttributes()
+	for k, v := range pp.HTMX.GetAttributes() {
+		htmxAttributes[k] = v
+	}
 	htmxAttributes["hx-replace-url"] = templ.SafeURL(pp.BuiltURL)
 	logrus.Infoln("hx-replace-url value", htmxAttributes["hx-replace-url"])
 
@@ -102,7 +104,7 @@ func Show(props PaginationItemProps) templ.Component {
 		var templ_7745c5c3_Var5 string
 		templ_7745c5c3_Var5, templ_7745c5c3_Err = templ.JoinStringErrs(props.AriaLabel)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `pkg/components/pagination/paginationitem/paginationitem.templ`, Line: 53, Col: 40}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `pkg/components/pagination/paginationitem/paginationitem.templ`, Line: 55, Col: 40}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var5))
 		if templ_7745c5c3_Err != nil {
