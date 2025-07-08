77051df92e0b9017b7c50c1bd8f9d5b096714ddd
user <user@email>
2025-07-08 18:11:45 +0100

    using children spread correctly

--- pkg/forms/select/select_templ.go
+++ pkg/forms/select/select_templ.go
@@ -211,7 +211,7 @@ func Show(props SelectProps) templ.Component {
 				}
 			}
 		}
-		templ_7745c5c3_Err = templ.GetChildren(ctx).Render(ctx, templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = templ_7745c5c3_Var1.Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
